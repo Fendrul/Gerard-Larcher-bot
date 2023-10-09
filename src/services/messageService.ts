9d89dedb20b1847e190dc6bccf79b10d0ee5a52e
Simon <simon.strykers@gmail.com>
2023-10-09 22:06:51 +0200

    getMessagesBetweenTwoIDs va a présent chercher les messages directement à l'endroit de l'id du message

--- src/services/messageService.ts
+++ src/services/messageService.ts
@@ -2,10 +2,12 @@ import {Collection, Message, TextChannel} from "discord.js";
 
 export class MessageService {
   private static instance: MessageService;
-  private static maxMessages: number;
+  private static messageInterval: number;
+  private static maxMessage: number;
 
   private constructor() {
-    MessageService.maxMessages = 100;
+    MessageService.messageInterval = 2;
+    MessageService.maxMessage = 100;
   }
 
   public static getInstance(): MessageService {
@@ -17,10 +19,8 @@ export class MessageService {
   }
 
   async getMessagesBetweenTwoIDs(channel: TextChannel, firstID: string, secondID: string): Promise<Collection<string, Message<true>>> {
-    let firstMesage: Message;
-    let secondMessage: Message;
-
-    const allMessages = await channel.messages.fetch({limit: MessageService.maxMessages});
+    let firstMesage: Message<true>;
+    let secondMessage: Message<true>;
 
     try {
       firstMesage = await channel.messages.fetch(firstID);
@@ -28,6 +28,16 @@ export class MessageService {
     } catch (error) {
       throw new Error("Message ID not found");
     }
+
+
+    const allMessages = await channel.messages.fetch({
+      limit: MessageService.maxMessage,
+      after: firstMesage.id,
+    });
+
+    console.log(allMessages);
+
+    allMessages.set(firstMesage.id, firstMesage);
 
     return allMessages.filter((message) => {
       return message.createdTimestamp >= firstMesage.createdTimestamp && message.createdTimestamp <= secondMessage.createdTimestamp;
